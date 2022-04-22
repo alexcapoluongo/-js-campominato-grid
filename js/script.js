2da316e19616de5c640cb2e6e8c0a4d478cd0873
alexcapoluongo <alexcapoluongo@icloud.com>
2022-04-22 17:32:37 +0200

    createElement added

--- js/script.js
+++ js/script.js
@@ -6,23 +6,45 @@
 //1.1 CREARE UN FOR DA 1 A 100
 //1.2 AGGIUNGERE 100 DIV A CONTAINER
 
-generateGridItem();
+containerContent();
 
-function generateGridItem() {
+function containerContent() {
+    // per ogni numero creo una grid item
     const gridContainer = document.querySelector(".grid-container");
-    for (let i = 1; i <= 100; i++) {
-        let number = 0;
-        number += i;
-        //inserisci in html con template
-        gridContainer.innerHTML += `<div class="grid-item">${number}</div>`;
 
-        const gridItem = document.querySelector(".grid-item");
-        gridItem.addEventListener("click",
-        function makeGridBlue() {
-            this.classList.add('active');
-        })
+    for (let i = 1; i <= 100; i++) {
+        
+        const domElement = generateGridItem(i);
+
+        
+        //append
+        gridContainer.append(domElement);
+        //inserisci in html elementi con template
+        // gridContainer.innerHTML += `<div class="grid-item">${number}</div>`;
+
+        //associa una variabile al dom e aggiungi con eventlistener una classe
+
+        // const gridItem = document.querySelector(".grid-item");
+        // gridItem.addEventListener("click",
+        // function() {
+        //     this.classList.add('active');
+        // })
     }    
     return gridContainer;
+}
+
+function generateGridItem(number) {
+    //crea una var associata a un div creato
+    const newGridItem = document.createElement("div");
+
+    //aggiungi numero all'interno del div
+    newGridItem.innerHTML = `${number}`;
+
+    //assegna classe grid item
+    newGridItem.classList.add('grid-item');
+
+    return newGridItem;
+
 }
 
 
